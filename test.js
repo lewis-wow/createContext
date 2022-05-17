4fe12b9f9f690f4b950613492d0b885104dfe2e6
Ludvík Prokopec <39367469+lewis-wow@users.noreply.github.com>
2022-05-17 17:47:19 +0200

    Create test.js

---
+++ test.js
@@ -0,0 +1,14 @@
+const ctx = createContext({ a: 1, b: "hello" }).bind({ a: 2, b: "world" });
+
+ctx((param1, param2) => {
+    console.assert(this.a === 2);
+    console.assert(this.b === "world");
+
+    console.assert(a === 1);
+    console.assert(b === "hello");
+
+    console.assert(param1 === 1);
+    console.assert(param2 === 2);
+
+    console.log("%cpassed!", "color: lime;");
+}, 1, 2);
